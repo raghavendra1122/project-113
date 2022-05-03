a476a00c6733217bb14869945026bdb30ce343b4
raghavendra1122 <104831134+raghavendra1122@users.noreply.github.com>
2022-05-03 15:48:23 +0530

    Add files via upload

---
+++ project 114/main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+ }
+
+ 
+function take_snapshot(){
+    save('mypictureImmage.png');
+}
